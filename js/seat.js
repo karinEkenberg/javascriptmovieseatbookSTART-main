7d9dae2dd38702e4555a3dc0b345adb03d1f490d
karinEkenberg <137770952+karinEkenberg@users.noreply.github.com>
2024-10-31 14:51:50 +0100

    selected seat works

--- js/seat.js
+++ js/seat.js
@@ -1,3 +1,9 @@
 export function Seats() {
   console.log("hejhej");
+  const changeSeat = document.querySelectorAll(".seat");
+  changeSeat.forEach((changeSeat) => {
+    changeSeat.addEventListener("click", function () {
+      changeSeat.classList.add("selected");
+    });
+  });
 }
