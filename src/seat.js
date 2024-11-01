1b5075620af42fd0e79c9d2b5697c5381123e113
karinEkenberg <137770952+karinEkenberg@users.noreply.github.com>
2024-11-01 11:57:33 +0100

    seats toggle working

--- src/seat.js
+++ src/seat.js
@@ -1,9 +1,8 @@
 export function Seats() {
-  console.log("hejhej");
   const changeSeat = document.querySelectorAll(".seat");
   changeSeat.forEach((changeSeat) => {
     changeSeat.addEventListener("click", function () {
-      changeSeat.classList.add("selected");
+      changeSeat.classList.toggle("selected");
     });
   });
 }
